e0753d5f192e1d61bdf51b9a1d3f73b315e051c1
PolarizedIons <polarizedions@gmail.com>
2021-02-18 18:27:53 +0200

    Fall back to username + discriminator when the user doesn't have a nickname

--- ButtBot/Utils/EmbedUtils.cs
+++ ButtBot/Utils/EmbedUtils.cs
@@ -1,4 +1,5 @@
 using ButtBot.Database.Models;
+using ButtBot.Extentions;
 using Discord;
 
 namespace ButtBot.Utils
@@ -10,8 +11,8 @@ namespace ButtBot.Utils
             return new EmbedBuilder()
                 .WithTitle("Buttcoin transfer report  ➡")
                 .WithThumbnailUrl(logo)
-                .AddField($"From: {fromUser.Nickname}", $"{fromAccount.Balance} buttcoins", true)
-                .AddField($"To: {toUser.Nickname}", $"{toAccount.Balance} buttcoins", true)
+                .AddField($"From: {fromUser.GetDisplayName()}", $"{fromAccount.Balance} buttcoins", true)
+                .AddField($"To: {toUser.GetDisplayName()}", $"{toAccount.Balance} buttcoins", true)
                 .AddField("Reason", reason, false)
                 .WithColor(Color.Green)
                 .WithFooter("ButtBot", logo)
@@ -21,7 +22,7 @@ namespace ButtBot.Utils
         public static EmbedBuilder CreateBalanceEmbed(IGuildUser user, ButtcoinAccount account, string logo)
         {
             return new EmbedBuilder()
-                .WithTitle($"{user.Nickname}'s buttcoin balance")
+                .WithTitle($"{user.GetDisplayName()}'s buttcoin balance")
                 .WithThumbnailUrl(user.GetAvatarUrl())
                 .AddField("Account Status", account.IsActive ? "Active" : "Inactive", true)
                 .AddField("Balance", $"{account.Balance} buttcoins", true)
@@ -33,7 +34,7 @@ namespace ButtBot.Utils
         public static EmbedBuilder CreateStatsEmbed(IGuildUser user, ButtcoinAccount account, string logo)
         {
             return CreateBalanceEmbed(user, account, logo)
-                .WithTitle($"{user.Nickname}'s buttcoin stats")
+                .WithTitle($"{user.GetDisplayName()}'s buttcoin stats")
                 .AddField("# Mined", $"{account.Stats.AmountMined} buttcoins", true)
                 .AddField("# Bruteforced", $"{account.Stats.AmountBruteforced} buttcoins", true)
                 .AddField("# Gifted", $"{account.Stats.AmountGifted} buttcoins", true)
